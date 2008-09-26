1fa9b868f4d99f4e40ea2b4f993ba0cae055d587
Vitaly Babiy <vbabiy86@gmail.com>
2008-09-26 01:04:00 -0400

    Fixed Conficts

--- settings.py
+++ settings.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD:settings.py
 import os
-=======
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
+
 # Django settings for django_blog project.
 
 DEBUG = True
@@ -13,17 +11,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-<<<<<<< HEAD:settings.py
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'django_blog'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
 DATABASE_PASSWORD = '23wesdxc'         # Not used with sqlite3.
-=======
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -31,21 +22,12 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 <<<<<<< HEAD:settings.py
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-=======
-# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-# although not all variations may be possible on all operating systems.
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
-<<<<<<< HEAD:settings.py
 # http://www.i18nguy.com/unicode/language-identifiers.html
-=======
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
@@ -58,14 +40,9 @@ USE_I18N = True
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = ''
 
-<<<<<<< HEAD:settings.py
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-=======
-# URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
@@ -74,11 +51,7 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-<<<<<<< HEAD:settings.py
 SECRET_KEY = '#zl&o*5vx^u)0b04uzya_#julf8o7vrg1c#4ohf0n#ip#!mf)t'
-=======
-SECRET_KEY = 'jekcgc=1!xh3&j#=-jfd5xtwp^vo=9&oqo+(1g8a@uld_*l21h'
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -91,22 +64,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-<<<<<<< HEAD:settings.py
-=======
-    'django.middleware.doc.XViewMiddleware',
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 )
 
 ROOT_URLCONF = 'django_blog.urls'
 
 TEMPLATE_DIRS = (
-<<<<<<< HEAD:settings.py
-    os.path.join(os.path.dirname(__file__)+"/templates"),
-=======
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
+    os.path.join(os.path.dirname(__file__), "/templates"),
 )
 
 INSTALLED_APPS = (
@@ -114,10 +77,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-<<<<<<< HEAD:settings.py
     'django.contrib.admin',
     'django_blog.blog',
     'django_blog.profiles'
-=======
->>>>>>> f9520ffc2c57d22eb23f009dee797c3f381170e7:settings.py
 )
