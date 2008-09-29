33bf7a45a01828defdb11c7be9ad7ec513fa3284
Vitaly Babiy <vbabiy86@gmail.com>
2008-09-28 21:06:38 -0400

    Fixed Query to show last post first

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 def recent_post(request):
-    current_posts=Post.objects.all()
+    current_posts=Post.objects.order_by("-date_published").all()
     
     context = RequestContext(request, {
         "posts": current_posts
@@ -19,4 +19,3 @@ def view_post(request, post_id):
     })
     
     return render_to_response("blog/view.html", context)
-    
